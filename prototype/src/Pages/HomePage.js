42b88ebac7ae6203e757ccfb449245d7be592400
ClementLegros <clement.legros.91220@gmail.com>
2022-04-12 11:21:18 +0200

    Début prototype

--- prototype/src/Pages/HomePage.js
+++ prototype/src/Pages/HomePage.js
@@ -1,7 +1,45 @@
+import { Link } from "react-router-dom";
+
 function HomePage() {
     return (
-        <div className="App">
-            <h1>mAQUETTE</h1>
+        <div className="flex flex-col justify-center bg-gray-300 items-center h-screen">
+            <h1 className="text-[30px] pb-8">Accueil</h1>
+            <div className="rounded-xl bg-gray-50 w-1/4 h-1/4 mb-8">
+                <h4 className="text-center pt-2">Soirées récentes :</h4>
+                <table class="table-fixed">
+                <thead>
+                    <tr>
+                    <th>Date</th>
+                    <th>Lieu</th>
+                    <th>Heure</th>
+                    <th>Durée</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                    <td>10/04/2022</td>
+                    <td>Orsay</td>
+                    <td>21H</td>
+                    <td>5H</td>
+                    </tr>
+                    <tr>
+                    <td>10/04/2022</td>
+                    <td>Orsay</td>
+                    <td>21H</td>
+                    <td>5H</td>
+                    </tr>
+                    <tr>
+                    <td>10/04/2022</td>
+                    <td>Orsay</td>
+                    <td>21H</td>
+                    <td>5H</td>
+                    </tr>
+                </tbody>
+                </table>
+            </div>
+            <div>
+            <Link to="/nouvellesoiree" className="rounded-xl bg-green-500 p-4">Nouvelle soirée</Link>
+            </div>
         </div>
     );
 }
--- prototype/src/Pages/NouvelleSoiree.js
+++ prototype/src/Pages/NouvelleSoiree.js
@@ -2,9 +2,72 @@
 
 function NouvelleSoiree() {
     return (
-        <div className="bg-red-100">
-            <h1 className="">Nouvelle soirée</h1>
-        </div>
+
+        <>
+        <div className="h-screen flex flex-col justify-center items-center bg-gray-300">
+            <p>Nouvelle soirée</p>
+            <div className="bg-gray-50 rounded-xl mx-12 my-12 w-1/4 h-1/4 pl-5 pt-2">
+                <p className="underline decoration-2 pb-5">Détails de la soirée</p>
+                <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+                    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                            <tr>
+                                <th>Date</th>
+                                <th>Lieu</th>
+                                <th>Heure</th>
+                                <th>Durée</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>25/04/2021</td>
+                                <td>Université Paris Saclay</td>
+                                <td>21h</td>
+                                <td>5 heures</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            <div className="bg-gray-50 rounded-xl mx-12 my-12 w-1/4 h-1/4 pl-5 pt-2">
+                <p className="underline decoration-2 pb-5">Liste invité</p>
+                <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+                    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                            <tr>
+                                <th>Nom</th>
+                                <th>Prénom</th>
+                                <th>Téléphone</th>
+                                <th>Mail</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Huge</td>
+                                <td>Babar</td>
+                                <td>0645782154</td>
+                                <td>hb@gmail.com</td>
+                            </tr>
+                            <tr>
+                                <td>Huge</td>
+                                <td>Babar</td>
+                                <td>0645782154</td>
+                                <td>hb@gmail.com</td>
+                            </tr>
+                            <tr>
+                                <td>Huge</td>
+                                <td>Babar</td>
+                                <td>0645782154</td>
+                                <td>hb@gmail.com</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div >
+        </>
+
+        
     )
 }
 
